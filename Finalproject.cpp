c8a305f891404d45ad2011f25b08397ace75687e
David Perez <davidcalebpb@gmail.com>
2019-12-13 17:09:37 -0800

    Code implementation and bug clean up

--- Finalproject.cpp
+++ Finalproject.cpp
@@ -7,10 +7,10 @@ using namespace std;
 class Registration {
 public:
 	//public functions overloaded with main() function variables
-	void minimizeCode(vector<string>, vector<int>, string, int, int);
+	void minimizeCode(vector<string>, vector<int>, string, int,unsigned int);
 	vector<string> studySubjects(int);
 	vector<int> unitSubjects(int);
-	void warning(char&, int, bool&, vector<int> unit);
+	void warning(char&, int, bool&, vector<int> unit,int);
 	void prompt(int);
 	void displaySummary(int);
 	void modify(char, int, bool&);
@@ -20,20 +20,19 @@ public:
 	int verifySelection(unsigned int&);
 
 	//public variables initilized to vector string values
-	vector <string> semesters = { "Spring", "Summer", "Fall" };
-	vector < string > studyField = { "Science", "Mathematics", "Arts", "Humanities" };
-	vector < string > science = { "Earth Science", "Biology", "Oceanography", "Geology", "Astronomy" };
-	vector < string > mathematics = { "Pre-Algebra", "Algebra", "Geometry", "Calculus I", "Calculus II" };
-	vector < string > arts = { "Music", "Painting", "Literature", "Sculpting", "Film" };
-	vector < string > humanities = { "Japenese", "Creative Writing", "Elementary French", "Hispanic Literature", "California History" };
+	const vector <string> semesters = { "Spring", "Summer", "Fall" };
+	const vector < string > studyField = { "Science", "Mathematics", "Arts", "Humanities" };
+	const vector < string > science = { "Earth Science", "Biology", "Oceanography", "Geology", "Astronomy" };
+	const vector < string > mathematics = { "Pre-Algebra", "Algebra", "Geometry", "Calculus I", "Calculus II" };
+	const vector < string > arts = { "Music", "Painting", "Literature", "Sculpting", "Film" };
+	const vector < string > humanities = { "Japenese", "Creative Writing", "Elementary French", "Hispanic Literature", "California History" };
 	vector <string> storeSubjects;
-	vector <int> unitScience = { 3, 4, 3, 3, 2 };
-	vector <int> unitMath = { 2, 4, 4, 4, 4 };
-	vector <int> unitArts = { 3, 3, 4, 3, 4 };
-	vector <int> unitHumanities = { 3, 4, 3, 4, 3 };
+	//public variables initilizied to vector integer values
+	const vector <int> unitScience = { 3, 4, 3, 3, 2 };
+	const vector <int> unitMath = { 2, 4, 4, 4, 4 };
+	const vector <int> unitArts = { 3, 3, 4, 3, 4 };
+	const vector <int> unitHumanities = { 3, 4, 3, 4, 3 };
 	vector <int> storeUnits;
-private:
-
 };
 
 int main()
@@ -47,16 +46,22 @@ int main()
 	unsigned int selection = 0;
 	bool quit = false;
 	char choice = '\0';
-	char dummyVar = '\0';
 
 	//object for Registration
 	Registration springClass;
 	Registration summerClass;
 	Registration fallClass;
-	//reuse class registration to create summerClass, and fallClass objects
+	//reuse class Registration to create springClass, summerClass, and fallClass objects
 
 	cout << "Which semester would you like to register for? \n1. Spring\n2. Summer\n3. Fall\nMake your selection: ";
 	cin >> semester;
+	while ((semester <= 0) || (semester > 3))
+	{
+		cin.clear();
+		cin.ignore();
+		cout << "Please select one of the options above: ";
+		cin >> semester;
+	}
 
 	for (;semester < 4; semester++)
 	{
@@ -75,13 +80,14 @@ int main()
 				subject = springClass.studySubjects(selection);
 				unit = springClass.unitSubjects(selection);
 				springClass.minimizeCode(subject, unit, compare, count, selection);
-				springClass.warning(choice, count, quit, unit);
+				springClass.warning(choice, count, quit, unit, semester);
 				springClass.decideSemester(choice, quit, semester); // choice to skip summer
 				springClass.modify(choice, selection, quit);
 			}
 			else if (semester == 2)
 			{
 				quit = false;
+				choice = '\0'; //reset choice
 				system("CLS"); //clear
 
 				summerClass.displaySemester(semester); // display the semester
@@ -95,13 +101,14 @@ int main()
 				subject = summerClass.studySubjects(selection);
 				unit = summerClass.unitSubjects(selection);
 				summerClass.minimizeCode(subject, unit, compare, count, selection);
-				summerClass.warning(choice, count, quit, unit);
+				summerClass.warning(choice, count, quit, unit, semester);
 				summerClass.decideSemester(choice, quit, semester); // choice to skip fall
 				summerClass.modify(choice, selection, quit);
 			}
 			else
 			{
 				quit = false;
+				choice = '\0'; // reset choice
 				system("CLS"); //clear
 
 				fallClass.displaySemester(semester); // display the semester
@@ -115,7 +122,7 @@ int main()
 				subject = fallClass.studySubjects(selection);
 				unit = fallClass.unitSubjects(selection);
 				fallClass.minimizeCode(subject, unit, compare, count, selection);
-				fallClass.warning(choice, count, quit, unit);
+				fallClass.warning(choice, count, quit, unit, semester);
 				//implement editing logic to ADD, Delete, Replace current selections
 				fallClass.modify(choice, selection, quit);
 			}
@@ -136,10 +143,9 @@ int main()
 	return 0;
 }
 
-void Registration::minimizeCode(vector<string> subject, vector<int> unit, string compare, int count, int selection) {
+void Registration::minimizeCode(vector<string> subject, vector<int> unit, string compare, int count, unsigned int selection) {
 	if (studyField.at(selection) == compare)
 	{
-		char dummyVar = '\0';
 		cout << endl;
 		for (unsigned int i = 0; i < subject.size(); i++)
 		{
@@ -149,20 +155,16 @@ void Registration::minimizeCode(vector<string> subject, vector<int> unit, string
 		count = 1;
 		cout << "Please make your class selection now: ";
 		cin >> selection;
-		cin.ignore(100, '\n');
+		while ((selection <= 0) || (selection > subject.size()))
+		{
+			cin.clear();
+			cin.ignore();
+			cout << "Your selection is invalid, please enter a # from 1 to " << subject.size() << " ";
+			cin >> selection;
+		}
 		selection -= 1;
-
-		if (selection >= 0 && selection <= 4)
-		{
-			storeSubjects.push_back(subject.at(selection));
-			storeUnits.push_back(unit.at(selection));
-		}
-		else
-		{
-			cout << "Out of range! Please select between 1 and 5. \n";
-			cout << "Press Enter to continue. ";
-			cin.get(dummyVar);
-		}
+		storeSubjects.push_back(subject.at(selection));
+		storeUnits.push_back(unit.at(selection));
 	}
 }
 vector<string> Registration::studySubjects(int selection) {
@@ -210,12 +212,38 @@ void Registration::prompt(int count) {
 	}
 	count = 1;
 }
-void Registration::warning(char& choice, int count, bool& quit, vector<int> unit) {
+void Registration::warning(char& choice, int count, bool& quit, vector<int> unit,int semester) {
 	if (storeSubjects.size() >= 4)
 	{
 		system("CLS");//clear
-
+	
 		cout << "We recommend that you only take 4 classes per semester, you have selected these classes: " << endl;
+		cout << "----------------------------------" << endl;
+
+		for (unsigned int i = 0; i < storeSubjects.size(); i++)
+		{
+			cout << count << ": " << left << setw(25) << storeSubjects.at(i) << storeUnits.at(i) << endl;
+			cout << "-----------------------------------" << endl;
+			count++;
+		}
+		count = 1;
+		cout << "\nWould you like to add more? (Y / N): ";
+		cin >> choice;
+		if (choice == 'N' || choice == 'n')
+		{
+			quit = true;
+			if (quit == true)
+			{
+				cout << "If you would like to DELETE or REPLACE one of the above classes type: (D) to delete / (R) to replace\nOtherwise type: (N): ";
+				cin >> choice;
+			}
+		}
+	}
+	else if ((storeSubjects.size() >= 2) && (semester == 2))
+	{
+		system("CLS");//clear
+
+		cout << "We recommend that you only take 2 classes during summer, you have selected these classes: " << endl;
 		cout << "----------------------------------" << endl;
 
 		for (unsigned int i = 0; i < storeSubjects.size(); i++)
@@ -240,7 +268,7 @@ void Registration::warning(char& choice, int count, bool& quit, vector<int> unit
 }
 void Registration::decideSemester(char choice, int quit, int &semester) {
 	if ((choice == 'N')||(choice == 'n')) {
-		cout << "Would you like to move to the next semester? (" << semesters.at(semester) << ") (Y) to continue (N) to skip " << semesters.at(semester) << ": " << endl;;
+		cout << "Would you like to move to the next semester? (" << semesters.at(semester) << ") (Y) to continue (N) to skip " << semesters.at(semester) << ": ";
 		cin >> choice;
 		if ((choice == 'N')||(choice == 'n')) {
 			semester++;
@@ -278,6 +306,22 @@ void Registration::displaySummary(int count) {
 	cout << left << setw(28) << "\nTotal Units: " << counter;
 	cout << endl;
 	cout << "_________________________________________________________________________________________" << endl;
+	if ((counter >= 12) && (counter <= 14))
+	{
+		cout << "You are considered a full time student because you have " << counter << " Units." << endl;
+		cout << "Please Note: if you are eligible for Cal Grant then you qualify to recieve $547 dollars per semester." << endl;
+		cout << "In addition, Chaffey College will grant you an SSCG grant of $647 dollars per semester.\n" << endl;
+	}
+	else if (counter >= 15)
+	{
+		cout << "You are considered a full time student because you have " << counter << " Units." << endl;
+		cout << "Please Note: if you are eligible for Cal Grant then you qualify to recieve $547 dollars per semester." << endl;
+		cout << "In addition, Chaffey College will grant you an SSCG grant of $2000 dollars per semester.\n" << endl;
+	}
+	else
+	{
+		cout << "You are not considered a full time student because you only have " << counter << " Units.\n" << endl;
+	}
 }
 int Registration::verifySelection(unsigned int &selection) {
 	while ((selection <= 0) || (selection > studyField.size()))
